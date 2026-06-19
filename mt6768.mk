4297c8864665346a6a773fb97bb2022a987e9245
7jari <zadigsm@outlook.com>
2026-06-19 13:06:25 +0700

    mt6768-common: switch to prebuilt HIDL USB service
    * Common AIDL service seems to break USB configuration settings

--- mt6768.mk
+++ mt6768.mk
@@ -378,7 +378,12 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.mediatek
+    android.hardware.usb@1.0.vendor \
+    android.hardware.usb@1.1.vendor \
+    android.hardware.usb@1.2.vendor \
+    android.hardware.usb@1.3.vendor \
+    android.hardware.usb.gadget@1.0.vendor \
+    android.hardware.usb.gadget@1.1.vendor
 
 # Vibrator
 PRODUCT_PACKAGES += \
